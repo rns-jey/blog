b3b3950cbf781c8c7be511f239aad44aa9903958
JM Arenas <rns.jey@gmail.com>
2021-11-26 20:43:34 +0800

    Rails generate model Comment with referencing to articles

--- db/migrate/20211126121401_create_comments.rb
+++ db/migrate/20211126121401_create_comments.rb
@@ -1,6 +1,11 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.boolean :toxic, default: false
+
+      t.references :article, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_033945) do
+ActiveRecord::Schema.define(version: 2021_11_26_121401) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 2021_11_13_033945) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.boolean "toxic", default: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
