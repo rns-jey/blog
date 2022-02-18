baee1294cc6b92be53c6c33ac5c763023550ad3d
JM Arenas <rns.jey@gmail.com>
2022-02-19 03:35:33 +0800

    Built inquiry CRUD controller

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,4 +1,54 @@
 class InquiryController < ApplicationController
+  before_action :set_inquiry, only:[:edit, :update, :destroy]
+  
   def index
+    @inquiries = Inquiry.all
+  end
+
+  def show
+    @inquiry = Inquiry.find(params[:id])
+  end
+
+  def new
+    @inquiry = Inquiry.new
+  end
+
+  def create
+    @inquiry = Inquiry.new(inquiry_params)
+
+    if @inquiry.save
+      redirect_to @inquiry
+    else
+      flash[:error] = "Blank field"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @inquiry.update(inquiry_params)
+      redirect_to @inquiry
+    else
+      flash[:error] = "Blank field"
+      render :edit
+    end
+  end
+
+  def destroy
+    @inquiry.destroy
+    redirect_to inquiries_path
+  end
+
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :message)
+  end
+
+  def set_inquiry
+    @inquiry = Inquiry.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_121401) do
+ActiveRecord::Schema.define(version: 2022_02_18_192445) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2021_11_26_121401) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "articles"
